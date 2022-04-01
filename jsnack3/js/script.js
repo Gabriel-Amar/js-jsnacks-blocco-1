071b0ad4e655d44347a0c3ed50d19b62546a9779
Gabriel-Amar <amariutei.gabriel1@gmail.com>
2022-04-01 11:19:02 +0200

    js: corretto JSnack3

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -7,3 +7,15 @@ console.log(miaArray);
 let risultato = miaArray[0] + miaArray[1]+ miaArray[2]+ miaArray[3];
 
 console.log(risultato)
+
+let somma = 0;
+
+let index = 0;
+
+while (index < risultato.length){
+    console.log("index: ", index)
+    console.log("conta numero: ", risultato[index]);
+    somma += parseInt(risultato[index])
+    console.log("somma ", somma);
+    index++
+}
